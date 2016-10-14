55aefeadb0cef8609b499eb998558f03fad83225
godsend <jcavat@gmail.com>
2016-10-14 20:05:49 +0200

    Prepare a MapService with functions signatures

--- app/core/map.service.ts
+++ app/core/map.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Map, MapType } from './map';
 
 @Injectable()
 export class MapService {
 
-  constructor() {
+  constructor(private http: Http) {
   }
+
+  addMap(
+    title: string = "",
+    height: number,
+    width: number,
+    mapType: MapType,
+    creatorId: number,
+    graphics: string = ""
+  ) {
+    // TODO: compute hash, id, date and return an Observable<Map>
+  }
+
+  map(id: number) {
+
+  }
+
+  maps() {
+
+  }
+
 
 }
