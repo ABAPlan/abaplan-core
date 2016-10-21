f8f641aed513d360a54acecd3a048bcfa2f5d08e
godsend <jcavat@gmail.com>
2016-10-21 14:07:17 +0200

    Add LayerService provider

--- app/city-map/city-map.module.ts
+++ app/city-map/city-map.module.ts
@@ -3,10 +3,11 @@ import { NgModule }           from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapService }       from '../core/map.service';
+import { LayerService }       from '../core/layer.service';
 
 @NgModule({
   imports:      [ CommonModule ],
   declarations: [ ],
-  providers:    [ MapService ]
+  providers:    [ MapService, LayerService ]
 })
 export class CityMapModule { }
