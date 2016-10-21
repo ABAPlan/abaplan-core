0930b56461dcb9be1f17a43837882684634610d3
godsend <jcavat@gmail.com>
2016-10-21 14:07:53 +0200

    Remove Default enum

--- app/core/layer.ts
+++ app/core/layer.ts
@@ -1,4 +1,4 @@
-export type LayerType = Default | City | Square | Osm;
+export type LayerType = City | Square | Osm;
 interface City { kind: "city"; }
 interface Square { kind: "square"; }
 interface Osm { kind: "osm"; }
