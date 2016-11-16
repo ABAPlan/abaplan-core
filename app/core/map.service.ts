7858c7ce92e4dba611feee1260e59e8d45c16965
Johner <pierre.johner@gmail.com>
2016-11-16 17:45:01 +0100

    Add SqlLite in map.service.ts

--- app/core/map.service.ts
+++ app/core/map.service.ts
@@ -3,20 +3,27 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import {OptionMap, AbaMap } from './map';
 import { LayerType } from './layer';
-
+import Sqlite = require('sqlite');
 
 @Injectable()
 export class MapService {
 
   private mapsUrl = "app/maps";
   private divId: Node | string = 'map-div';
+  private database: any;
 
   constructor(private http: Http) {
+
+    (new Sqlite("audiotaczhadmin")).then(db => {
+        this.database = db;
+     }, error => {
+          console.log("OPEN DB ERROR", error);
+     });
   }
 
   add(abaMap: AbaMap) {
     /*
-
+    SQL lite with angular 2
      Comment (JCA) : All these attributes could be access from AbaMap (extending ArcgisMap)
 
      title: string = "",
@@ -25,9 +32,25 @@ export class MapService {
      layerType: LayerType,
      creatorId: number,
      graphics: string = ""
+     esri this.graphics
      )
      */
+     var pub= 1; //??????
+     var city = 0;
 
+     var jsonGraph ="";
+     var i = 0;
+     while(typeof abaMap.graphics[i] !== 'undefined'){
+       i++;
+       jsonGraph += abaMap.graphics[i].ToJson;
+     }
+
+     var insert = "INSERT INTO people (uid,creatorId,public,title,width,height,extent,hash,graphics,city,creationDate) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+     this.database.execSQL(insert, [abaMap.uid,abaMap.owner,pub,abaMap.title,abaMap.width,abaMap.height,abaMap.extent,abaMap.hash,jsonGraph,city,abaMap.dateCreation]).then(id => {
+           console.log("INSERT OK", id);
+       }, error => {
+           console.log("INSERT ERROR", error);
+       });
   }
 
   map(id: number): Observable<OptionMap> {
