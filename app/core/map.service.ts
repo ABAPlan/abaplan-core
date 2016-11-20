986747e1475a437e7dd6bf7e22b24bb1d11da491
Johner <pierre.johner@gmail.com>
2016-11-20 21:22:46 +0100

    Add select and delet

--- app/core/map.service.ts
+++ app/core/map.service.ts
@@ -3,8 +3,9 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import {OptionMap, AbaMap } from './map';
 import { LayerType } from './layer';
+import Graphic = require('esri/graphic');
 var sqlite3 = require('sqlite3').verbose();
-
+//import { sqlite3 } from 'sqlite3';
 
 @Injectable()
 export class MapService {
@@ -15,7 +16,7 @@ export class MapService {
 
   constructor(private http: Http) {
     this.db = new sqlite3.Database(':memory:');
-    this.db.run("CREATE TABLE `maps` ( `uid` INTEGER PRIMARY KEY AUTOINCREMENT, `creatorId` INTEGER, `public` INTEGER, `title` TEXT, `width` INTEGER, `height` INTEGER, `extent` TEXT, `hash` TEXT, `graphics` TEXT, `city` INTEGER, `creationDate` TEXT );");
+    this.db.run("CREATE TABLE `maps` ( `uid` INTEGER PRIMARY KEY AUTOINCREMENT, `creatorId` INTEGER, `public` INTEGER, `title` TEXT, `width` INTEGER, `height` INTEGER, `extent` TEXT, `hash` TEXT, `graphics` TEXT, `city` INTEGER, `creationDate` TEXT )");
   }
 
   add(abaMap: AbaMap) {
@@ -27,9 +28,28 @@ export class MapService {
      let jsonGraph ="";
      abaMap.graphics.graphics.forEach((graphic) => jsonGraph+=graphic.toJson());
 
-     let stmt = this.db.prepare("INSERT INTO people (uid,creatorId,public,title,width,height,extent,hash,graphics,city,creationDate) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+     // Save Layer Map ?
+
+     let stmt = this.db.prepare("INSERT INTO maps (uid,creatorId,public,title,width,height,extent,hash,graphics,city,creationDate) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
      stmt.run(abaMap.uid,abaMap.owner,pub,abaMap.title,abaMap.width,abaMap.height,abaMap.extent,abaMap.hash,jsonGraph,city,abaMap.dateCreation);
      stmt.finalize();
+  }
+
+  select(id :number):AbaMap{
+    let abaMap: AbaMap;
+    this.db.each("SELECT * FROM maps WHERE uid = ${id}", function(err, row) {
+      abaMap = new AbaMap(this.divId,row.extent);
+      abaMap.uid = row.uid;
+      abaMap.height = row.height;
+      abaMap.width = row.width;
+      abaMap.title = row.title;
+      abaMap.owner = row.creatorId;
+      abaMap.hash = row.hash;
+      abaMap.dateCreation = row.creationDate;
+      const json: any = JSON.parse(row.graphics);
+      json.graphics.forEach( (graphic) => abaMap.graphics.add(new Graphic(graphic)));
+    });
+    return abaMap;
   }
 
   map(id: number): Observable<OptionMap> {
@@ -43,6 +63,8 @@ export class MapService {
     return this.http.get(this.mapsUrl).map( (r: Response) => r.json().data as OptionMap[] );
   }
 
-  delete(id: number) {}
+  delete(id: number) {
+    this.db.run("DELETE FROM maps WHERE uid = ${id}");
+  }
 
 }
