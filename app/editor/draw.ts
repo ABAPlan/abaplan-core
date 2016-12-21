07dc3745a1162132899b87ad9763bea12528915f
godsend <jcavat@gmail.com>
2016-12-21 12:27:58 +0100

    Improve pedestrian pathways algorithm

--- app/editor/draw.ts
+++ app/editor/draw.ts
@@ -97,43 +97,9 @@ export class DrawInfoPedestrian implements DrawInfo {
     this.createPedestrianPathway(A, B, event.geometry.spatialReference, map);
   }
 
-  /**
-  * Calcul de la prochaine coordonnées d'un point du passage
-  * Piétons tel que la distance soit toujours constante
-  * Résoudre les équations y = mx + k et d = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2))
-  * (rch)
-  */
-  nextCoord (A, l, m, k, D1, D2) {
-    var a = Math.pow(m, 2) + 1;
-    var b = (2 * k * m) - (2 * A.y * m) - (2 * A.x);
-    var c = Math.pow(k, 2) - (2 * A.y * k) + Math.pow(A.x, 2) + Math.pow(A.y, 2) - Math.pow(l, 2);
 
-    var delta = Math.pow(b, 2) - (4 * a * c);
-    var x;
-    var y;
-
-    if (delta < 0) {
-      console.log("pas de solution");
-    } else if (delta == 0) {
-      x = (-b) / (2 * a);
-    } else {
-      var p1 = ((-b) - Math.sqrt(delta)) / (2 * a);
-      var p2 = ((-b) + Math.sqrt(delta)) / (2 * a);
-      if (D2.x - D1.x >= 0) {
-        x = (p1 > p2) ? p1 : p2;
-      } else {
-        x = (p1 < p2) ? p1 : p2;
-      }
-    }
-
-    y = m * x + k;
-
-    return { x: x, y: y };
-  }
-
-
-  /* no comment by the original author
-   * (rch)
+  /** This feature draw a pedetrian pathway on the map
+   *  (jca)
    */
   createPedestrianPathway (origin, destination, spatialRef, arcgisMap) {
 
@@ -147,121 +113,37 @@ export class DrawInfoPedestrian implements DrawInfo {
 
     const lengthAB = norm(AB);
     const lengthPerpAB = norm(perpAB);
+
+    let nbIter = Math.floor( lengthAB / 2.5 );
+    nbIter = nbIter % 2 == 0 ? nbIter+1 : nbIter;
+
     const unitPerpAB = multVec(pedestrianWidth/lengthPerpAB, perpAB);
-    const unitAB = multVec(pedestrianWidth/lengthAB, AB);
+    const unitAB = multVec(1/nbIter, AB);
 
-    const nbIter = Math.floor( lengthAB / 2.5 );
+    let A_ = clone(A);
+
     _.range(nbIter).forEach(
-      index => console.log(index)
+      index => {
 
+        let C1 = subVec(A_, unitPerpAB);
+        let C2 = addVec(A_, unitPerpAB);
+        A_ = addVec(A_, unitAB);
+        let C3 = addVec(A_, unitPerpAB);
+        let C4 = subVec(A_, unitPerpAB);
 
+        const geometry: Polygon = new Polygon([[C1.x, C1.y], [C2.x, C2.y], [C3.x, C3.y], [C4.x, C4.y]]);
+        geometry.setSpatialReference(spatialRef);
 
+        const symbol = (index+1) % 2 ?
+          new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, null, new Color([0, 0, 0, 1])) :
+          new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, null, new Color([255, 255, 255, 1]));
 
-
-
-
-
-
+        arcgisMap.graphics.add(new Graphic(geometry, symbol, {
+          "shape": this.geometryType,
+          "texture": symbol,
+          "passage_pieton": true
+        }));
+      }
     );
-
-    let C1 = subVec(A, unitPerpAB);
-    let C2 = addVec(A, unitPerpAB);
-    let A_ = addVec(A, unitAB);
-    let C3 = addVec(A_, unitPerpAB);
-    let C4 = subVec(A_, unitPerpAB);
-
-    const geometry: Polygon = new Polygon([[C1.x, C1.y], [C2.x, C2.y], [C3.x, C3.y], [C4.x, C4.y]]);
-    geometry.setSpatialReference(spatialRef);
-
-    const symbol: SimpleFillSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, null, new Color([0, 0, 0, 1]));
-
-    arcgisMap.graphics.add(new Graphic(geometry, symbol, {
-      "shape": this.geometryType,
-      "texture": symbol,
-      "passage_pieton": true
-    }));
-
   }
-
-
-
-
-
-
-    /*
-
-
-
-    // Distance entre les deux points composant le segment de droite dessiné
-    var AB = Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
-
-    console.log("--------------");
-    console.log(AB);
-
-
-    // Pente entre les points A et B
-    var m = (B.y - A.y) / (B.x - A.x);
-    var k = A.y - (m * A.x); // ordonnée à l'origine de la droite décrite par les points A et B
-
-    // longueur et largeur d'une bande du passage piétons
-    let pedestrianFillSizeZoom = this.pedestrianFillSize - arcgisMap.getZoom();
-    var l = 2.5 * pedestrianFillSizeZoom;
-    var h = 5 * pedestrianFillSizeZoom;
-
-    // Théorème de Thalès
-    var x = h * (B.y - A.y) / AB;
-    var y = h * (B.x - A.x) / AB;
-
-
-
-    //console.log(Math.round(AB / l));
-    let nbRectangles = Math.round(AB / l);
-    nbRectangles = nbRectangles % 2 == 0 ? nbRectangles + 1 : nbRectangles;
-
-    var C = { x: A.x - x, y: A.y + y };
-    var D = { x: 0, y: 0 };
-    var E = { x: 0, y: 0 };
-    var F = { x: A.x + x, y: A.y - y };
-    var geometries = [];
-    for (var i = 0; i < nbRectangles ; i++) {
-      // Calcul des points du rectangle autour du segment de droite
-
-      k = C.y - (m * C.x);
-      C = this.nextCoord(C, l, m, k, A, B);
-      D = this.nextCoord(C, l, m, k, A, B);
-
-      k = F.y - (m * F.x);
-      F = this.nextCoord(F, l, m, k, A, B);
-      E = this.nextCoord(F, l, m, k, A, B);
-
-      // Création du rectangle autour de la droite
-      var geometry = new Polygon([[C.x, C.y], [D.x, D.y], [E.x, E.y], [F.x, F.y]]);
-      geometry.setSpatialReference(spatialRef);
-
-      let symbol = (i+1)%2 ?
-        new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, null, new Color([0, 0, 0, 1])) :
-        new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, null, new Color([255, 255, 255, 1]));
-
-
-      geometries.push(geometry);
-
-
-     // arcgisMap.graphics.add(new Graphic(geometry, symbol, { "shape": this.geometryType, "texture": symbol, "passage_pieton": true }));
-
-  /*
-    }
-
-    //////// EditTools.ppAdded();
-
-    console.log(geometries.length + " polygon required to draw this pedestrian pathways");
-    console.log(geometries.length);
-
-    // Créé une geometrie avec tous les polygones, mais on ne peut plus avoir une alternance de noir/blanc
-    // puisqu'un seul polygone avec un symbole
-    //geometry = geometryEngine.union(geometries);
-    //this.arcgisMap.graphics.add(new Graphic(geometry, symbol, { "shape": this.currentToolname, "texture": this.fillType, "passage_pieton": true }));
-
-  }
-
-   */
 }
