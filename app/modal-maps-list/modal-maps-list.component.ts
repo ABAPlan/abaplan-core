d16d5118f2df2abca2acebf6d7a7ffe608093568
godsend <jcavat@gmail.com>
2017-02-21 17:05:23 +0100

    Add pagination for tables

--- app/modal-maps-list/modal-maps-list.component.ts
+++ app/modal-maps-list/modal-maps-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { MapService } from "../core/map.service";
 import { OptionMap } from "../core/map";
-
+import * as _ from "lodash";
 
 @Component({
   selector: 'aba-modal-maps',
@@ -16,12 +16,15 @@ export class ModalMapComponent {
   private maps: OptionMap[] = [];
   private filteredMaps: OptionMap[] = this.maps;
   private queryInputValue: string = "";
+  private activePage = 1;
+  private nbPaginations = 0;
 
   constructor(private mapService: MapService) {
     mapService.maps().subscribe(
       (maps : OptionMap[]) => {
         this.maps = maps;
-        this.filteredMaps = maps;
+        this.filteredMaps = maps.slice(0, 10);
+        this.nbPaginations = Math.floor(this.maps.length/10);
       },
       (error) => {
         console.log(error);
@@ -45,15 +48,28 @@ export class ModalMapComponent {
 
   private onChange(query: string): void {
     if (query !== ""){
-      this.filteredMaps = this.maps.filter( m => m.title.toLowerCase().includes(query.toLowerCase()) || m.uid.toString().includes(query));
+      this.filteredMaps =
+        this
+          .maps
+          .filter( m => m.title.toLowerCase().includes(query.toLowerCase()) || m.uid.toString().includes(query));
+      this.nbPaginations = Math.floor(this.filteredMaps.length/10);
+
+      this.filteredMaps = this.filteredMaps.slice(0, 10);
+
     }else{
-      this.filteredMaps = this.maps;
+      this.filteredMaps =
+        this.maps.slice(0, 10);
+      this.nbPaginations = Math.floor(this.maps.length/10);
     }
   }
 
   private onClick(id: number): void {
     this.close();
     this.onSelectChoice.emit(id);
+  }
+
+  private range(n): [number] {
+    return _.range(1, n+1);
   }
 
 }
