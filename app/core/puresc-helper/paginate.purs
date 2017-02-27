59c5e1d2fee706d0dc860ad77537c16eabc3ce7d
godsend <jcavat@gmail.com>
2017-02-27 14:19:45 +0100

    Change separation string

--- app/core/puresc-helper/paginate.purs
+++ app/core/puresc-helper/paginate.purs
@@ -11,11 +11,11 @@ paginate :: Int -> Int -> Array String
 paginate page activep
     | page == 0                 = []
     | page <= 10                = mapToString $ 1..page
-    | page > 10 && activep <= 5 = (mapToString $ 1..7) <> [".."] <> (mapToString [page-1, page])
-    | activep >= page -5        = (mapToString $ 1..2) <> [".."] <> (mapToString $ (page-6)..page)
+    | page > 10 && activep <= 5 = (mapToString $ 1..7) <> ["..."] <> (mapToString [page-1, page])
+    | activep >= page -5        = (mapToString $ 1..2) <> ["..."] <> (mapToString $ (page-6)..page)
     | otherwise                 = (mapToString $ 1..2)
-                                    <> [".."]
+                                    <> ["..."]
                                     <> (mapToString $ (activep-1)..(activep+2))
-                                    <> [".."]
+                                    <> ["..."]
                                     <> (mapToString [page-1, page])
 
