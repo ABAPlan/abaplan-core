67f8ed827e8118c7377119b99a14b50f44604965
Maxime Burri <maxime.burri@gmail.com>
2017-02-28 17:30:36 +0100

    Load Google Maps API

--- app/touchpad/touchpad.component.ts
+++ app/touchpad/touchpad.component.ts
@@ -5,6 +5,10 @@ import { MapService } from '../core/map.service';
 import { OptionMap, AbaMap } from '../core/map';
 import { MapComponent } from '../map/map.component'
 import { GOOGLE_GEOCODE_KEY } from './secret'
+import WebMercatorUtils = require('esri/geometry/webMercatorUtils');
+import Geometry = require('esri/geometry/Geometry');
+import Point = require('esri/geometry/Point')
+import googleMaps = require("google-maps"); 
 
 @Component({
   selector: 'aba-touchpad',
@@ -25,11 +29,18 @@ export class TouchpadComponent {
   ngOnInit() {
     // (+) converts string 'id' to a number
     let id = +this.route.snapshot.params['id'];
+    googleMaps.KEY = GOOGLE_GEOCODE_KEY;
+    googleMaps.load();
 
     this.service.map(id)
       .subscribe((optionMap: OptionMap) => {
         this.map.initMap(optionMap, {kind:"osm"});
         this.map.map.disableMapNavigation();
+        this.map.map.on("click", (event:any) => {
+          let point : Point = <Point> WebMercatorUtils.webMercatorToGeographic(event.mapPoint);
+          let p = new google.maps.LatLng(point.y, point.x);
+          console.log(p);
+        })
       }
     );
   }
