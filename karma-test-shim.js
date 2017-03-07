833a6b6f4ce46f933bd1db0ec53d9f3bab2f3e3c
godsend <jcavat@gmail.com>
2017-03-07 18:00:33 +0100

    Add karma config files

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -1,89 +1,38 @@
-// #docregion
-// /*global jasmine, __karma__, window*/
-Error.stackTraceLimit = 0; // "No stacktrace"" is usually best for app testing.
+Error.stackTraceLimit = Infinity;
 
-// Uncomment to get full stacktrace output. Sometimes helpful, usually not.
-// Error.stackTraceLimit = Infinity; //
+require('core-js/client/shim');
+require('reflect-metadata');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+require('ts-helpers');
 
-var builtPath = '/base/app/';
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
 
-__karma__.loaded = function () { };
+/*
+ Ok, this is kinda crazy. We can use the the context method on
+ require that webpack created in order to tell webpack
+ what files we actually want to require or import.
+ Below, context will be a function/object with file names as keys.
+ using that regex we are saying look in client/app and find
+ any file that ends with '.spec.ts' and get its path. By passing in true
+ we say do this recursively
+ */
+var appContext = require.context('./', true, /\.spec\.ts/);
 
-function isJsFile(path) {
-  return path.slice(-3) == '.js';
-}
+// get all the files, for each file, call the context function
+// that will require the file and load it up here. Context will
+// loop and require those spec files here
+appContext.keys().forEach(appContext);
 
-function isSpecFile(path) {
-  return /\.spec\.(.*\.)?js$/.test(path);
-}
+// Select BrowserDomAdapter.
+// see https://github.com/AngularClass/angular2-webpack-starter/issues/124
+// Somewhere in the test setup
+var testing = require('@angular/core/testing');
+var browser = require('@angular/platform-browser-dynamic/testing');
 
-function isBuiltFile(path) {
-  return isJsFile(path) && (path.substr(0, builtPath.length) == builtPath);
-}
-
-var allSpecFiles = Object.keys(window.__karma__.files)
-  .filter(isSpecFile)
-  .filter(isBuiltFile);
-
-System.config({
-  baseURL: '/base',
-  // Extend usual application package list with test folder
-  packages: { 'testing': { main: 'index.js', defaultExtension: 'js' } },
-
-  // Assume npm: is set in `paths` in systemjs.config
-  // Map the angular testing umd bundles
-  map: {
-    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-    '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-    '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-    '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
-  },
-});
-
-System.import('systemjs.config.js')
-  .then(importSystemJsExtras)
-  .then(initTestBed)
-  .then(initTesting);
-
-/** Optional SystemJS configuration extras. Keep going w/o it */
-function importSystemJsExtras(){
-  return System.import('systemjs.config.extras.js')
-  .catch(function(reason) {
-    console.log(
-      'Warning: System.import could not load the optional "systemjs.config.extras.js". Did you omit it by accident? Continuing without it.'
-    );
-    console.log(reason);
-  });
-}
-
-function initTestBed(){
-  return Promise.all([
-    System.import('@angular/core/testing'),
-    System.import('@angular/platform-browser-dynamic/testing')
-  ])
-
-  .then(function (providers) {
-    var coreTesting    = providers[0];
-    var browserTesting = providers[1];
-
-    coreTesting.TestBed.initTestEnvironment(
-      browserTesting.BrowserDynamicTestingModule,
-      browserTesting.platformBrowserDynamicTesting());
-  })
-}
-
-// Import all spec files and start karma
-function initTesting () {
-  return Promise.all(
-    allSpecFiles.map(function (moduleName) {
-      return System.import(moduleName);
-    })
-  )
-  .then(__karma__.start, __karma__.error);
-}
+testing.TestBed.initTestEnvironment(browser.BrowserDynamicTestingModule, browser.platformBrowserDynamicTesting());
