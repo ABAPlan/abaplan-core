a80c2fa5c4c09a64bc5d0318fd80a9b339af1e69
godsend <jcavat@gmail.com>
2017-03-08 10:36:41 +0100

    Delete exclusion rules for tsfiles

--- webpack.config.js
+++ webpack.config.js
@@ -36,9 +36,8 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.ts$/,
+                test: /\.tsx?$/,
                 loaders: ['ts-loader', 'angular2-template-loader'],
-                exclude: [/\.(spec|e2e)\.ts$/]
             },
             {
                 test: /\.(html|css)$/,
