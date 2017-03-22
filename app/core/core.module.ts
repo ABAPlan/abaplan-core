f55260c5b23315d949aea2043bdb090fb4fc4f19
godsend <jcavat@gmail.com>
2017-03-22 12:51:34 +0100

    Remove dead code

--- app/core/core.module.ts
+++ app/core/core.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule, JsonpModule } from '@angular/http';
 
-
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-
 @NgModule({
   imports: [
     CommonModule,
