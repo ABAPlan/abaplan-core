6ffb5a8e69f81fcaf07e93415d8c091fe5ebc004
godsend <jcavat@gmail.com>
2017-03-22 13:13:35 +0100

    Add good practices link

--- app/core/core.module.ts
+++ app/core/core.module.ts
@@ -1,3 +1,4 @@
+// good practices: https://angular.io/styleguide#!#-a-id-04-11-a-core-feature-module
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule, JsonpModule } from '@angular/http';
