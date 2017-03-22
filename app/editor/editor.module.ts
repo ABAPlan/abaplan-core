6badaa223f88d5a5de2e8f6629802a398f682ecd
godsend <jcavat@gmail.com>
2017-03-22 17:08:01 +0100

    Add modules for Editor and Touchpad

---
+++ app/editor/editor.module.ts
@@ -0,0 +1,12 @@
+import { NgModule }           from '@angular/core';
+
+//import { CommonModule } from '@angular/common';
+
+import { MapService }       from '../core/map.service';
+
+@NgModule({
+  imports:      [ ],
+  declarations: [ ],
+  providers:    [ MapService ]
+})
+export class EditorModule { }
