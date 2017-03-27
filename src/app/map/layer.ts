009d4a5d595559f920d9ce825878f7c2437b69cc
godsend <jcavat@gmail.com>
2017-03-27 15:22:09 +0200

    Replace SipleLineSymbol by default value

--- src/app/map/layer.ts
+++ src/app/map/layer.ts
@@ -67,14 +67,14 @@ const surface = {
   ]
 };
 
-const HARD_SYMBOL = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, new SimpleLineSymbol(), new Color('black'));
+const HARD_SYMBOL = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, SimpleLineSymbol.STYLE_NULL, new Color('black'));
 
 const url_traitilles = require("file?name=./assets/[name].[ext]!./../core/img/traitilles.png");
 const url_cercle = require("file?name=./assets/[name].[ext]!./../core/img/cercle.png");
 
-const BUILDING_SYMBOL = new PictureFillSymbol(url_traitilles, new SimpleLineSymbol(), 15, 15);
-const WATER_SYMBOL = new PictureFillSymbol(url_cercle, new SimpleLineSymbol(), 15, 15);
-const GREEN_SYMBOL = new PictureFillSymbol(url_traitilles, new SimpleLineSymbol(), 25, 25);
+const BUILDING_SYMBOL = new PictureFillSymbol(url_traitilles, SimpleLineSymbol.STYLE_NULL, 15, 15);
+const WATER_SYMBOL = new PictureFillSymbol(url_cercle, SimpleLineSymbol.STYLE_NULL, 15, 15);
+const GREEN_SYMBOL = new PictureFillSymbol(url_traitilles, SimpleLineSymbol.STYLE_NULL, 25, 25);
 
 const URL_FEATURE_LAYER = "https://hepiageo.hesge.ch/arcgis/rest/services/audiotactile/audiotactile/FeatureServer/";
 const URL_FEATURE_LAYER_SURFACE = URL_FEATURE_LAYER + '3';
