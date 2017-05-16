eb569f76237bcbabf37d1452ffc4ee9ba78d7956
JohnerPierre <pierre.johner@gmail.com>
2017-05-16 17:40:42 +0200

    Add Comment of itinerary service

--- src/app/core/kml.service.ts
+++ src/app/core/kml.service.ts
@@ -6,7 +6,6 @@ import * as fileSaver from 'file-saver';
 
 interface Point{name:string | undefined,lat:number | undefined,lng:number | undefined};
 
-
 @Injectable()
 export class KmlService {
 
@@ -17,17 +16,20 @@ export class KmlService {
   constructor() {
    }
 
+   /** Delete value in the current point */
   private emtpylastPoint(){
     this.lastPoint.lng = undefined;
     this.lastPoint.lat = undefined;
     this.lastPoint.name = undefined;
   }
 
+  /** Set current point */
   public currentPoint(longitude:number,latitude:number):void{
     this.lastPoint.lat = latitude;
     this.lastPoint.lng = longitude;
   }
 
+  /** Add Current Point to Datas with a bool of sucess */
   public addCurrentPoint(name:string):Boolean{
     if(this.lastPoint.lng == undefined)
       return false;
@@ -38,6 +40,7 @@ export class KmlService {
     return true;
   }
 
+  /** Delete Last Point (only once) with a bool of sucess */
   public deletLastPoint():Boolean{
     if(!this.isDeletable)
       return false;
@@ -47,13 +50,14 @@ export class KmlService {
     return true;
   }
 
+  /** DownLoad kml file with a bool of sucess */
   public toKml(nameFile:string):Boolean{
     if(this.datas.length <1)
       return false;
 
     let geojsonObject = geoJson.parse(this.datas, {Point: ['lat', 'lng']});
     let kmlNameDescription = tokml(geojsonObject, {
-          documentName: 'Initeraire'
+          documentName: 'Itinerary'
     });
 
     let file = new Blob([kmlNameDescription], { type: 'text/kml;charset=utf-8' });
@@ -61,6 +65,7 @@ export class KmlService {
     return true;
   }
 
+  /** Reset all var */
   public endCurrentSession():void{
     this.datas = [];
     this.emtpylastPoint();
