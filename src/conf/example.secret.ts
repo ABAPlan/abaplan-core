2204843f620c75878d7ab4b4729e842cb3abf05a
Robin Chappatte <robin.chappatte@gmail.com>
2018-03-20 14:43:38 +0100

    Move the GOOGLE_GEOCODE_KEY in a "conf" folder

--- src/conf/example.secret.ts
+++ src/conf/example.secret.ts
@@ -1,4 +1,4 @@
-// TODO before compile : 
+// TODO before compile :
 // 1) rename example.secret.ts to secret.ts
 // 2) enter google geocode key instead of "<enter the key>"
 export let GOOGLE_GEOCODE_KEY = "<enter the key>";
